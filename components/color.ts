757abf21c129da1721dfc800dd72e41e5c2006cb
Xylight <james@xylight.us>
2023-05-30 11:49:42 -0700

    design: Remove glow

--- components/color.ts
+++ components/color.ts
@@ -1,5 +1,5 @@
 export enum ButtonColor {
-    'accent' = 'text-white from-primary to-secondary bg-gradient-to-br hover:brightness-110 transition-all shadow-md  shadow-secondary/80',
+    'accent' = 'text-white from-primary to-secondary bg-gradient-to-br hover:brightness-110 transition-all',
     'primary' = 'text-white bg-black border border-black hover:bg-transparent hover:text-inherit dark:text-black dark:bg-white dark:border-white',
     'secondary' = 'border border-transparent hover:bg-black/10 hover:dark:bg-white/10',
 }
