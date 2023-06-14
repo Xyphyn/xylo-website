96dda6bfe0bc197a344a3acd12415fbdd9475f05
Xylight <james@xylight.us>
2023-06-14 11:25:33 -0700

    add error i am so tired

--- server/api/oauth2.get.ts
+++ server/api/oauth2.get.ts
@@ -31,15 +31,14 @@ export default defineEventHandler(async (e) => {
     const json = await res.json()
 
     if (res.status >= 400 || !json || !json.access_token) {
-        console.log(json)
+        throw Error(JSON.stringify(json))
+
         return {
             message: `Error during authorization`,
         }
     }
 
-    setCookie(e, 'discord-token', json.access_token, {
-        httpOnly: true,
-    })
+    setCookie(e, 'discord-token', json.access_token, {})
 
     // nice little hack because there's no easy way to redirect
     return sendRedirect(e, '/dashboard', 300)
